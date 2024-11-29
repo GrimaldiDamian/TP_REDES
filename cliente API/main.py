67de4ad7a59096164113826139c6f7a7c3bca73e
GrimaldiDamian <grimaldidamian001@gmail.com>
2024-11-29 17:42:29 -0300

    Creación del menu, y que se redirija a la opcion correspondiente

---
+++ cliente API/main.py
@@ -0,0 +1,58 @@
+def obtener_animes():
+    pass
+
+def obtener_descripcion():
+    pass
+
+def obtener_clasificacion():
+    pass
+
+def obtener_categoria():
+    pass
+
+def obtener_estudio():
+    pass
+
+def obtener_episodios():
+    pass
+
+def actualizar_episodios():
+    pass
+
+def actualizar_descripcion():
+    pass
+
+def actualizar_nombre():
+    pass
+
+def eliminar_anime():
+    pass
+
+def menu():
+    while True:
+        op = int(input("1. Obtener Animes\n2. Obtener descripcion\n3. Obtener clasificacion\n4. Obtener categoria\n5. Obtener estudio\n6. Obtener episodios\n7. Actualizar episodios\n8. Actualizar descripcion\n9. Actualizar nombre\n10. Eliminar anime\n11. Salir\nIngrese opcion: "))
+        
+        if op == 1:
+            print(obtener_animes())
+        elif op == 2:
+            print(obtener_descripcion())
+        elif op == 3:
+            print(obtener_clasificacion())
+        elif op == 4:
+            print(obtener_categoria())
+        elif op == 5:
+            print(obtener_estudio())
+        elif op == 6:
+            print(obtener_episodios())
+        elif op == 7:
+            print(actualizar_episodios())
+        elif op == 8:
+            print(actualizar_descripcion())
+        elif op == 9:
+            print(actualizar_nombre())
+        elif op == 10:
+            print(eliminar_anime())
+        elif op == 11:
+            break
+
+menu()
