a65c497486274994dc6ea3b5570aa75c2aefa2aa
GrimaldiDamian <grimaldidamian001@gmail.com>
2024-11-30 00:06:16 -0300

    Agregación de comentarios + las funciones de los gets terminadas

--- cliente API/main.py
+++ cliente API/main.py
@@ -2,7 +2,12 @@ import requests
 
 url = "http://localhost:8000/animes/"
 
-def obtener_animes():
+def obtener_animes() -> dict:
+    """
+    Función que obtiene los nombres de los animes
+    Returns:
+        [json]: [json con los nombres de los animes]
+    """
     try:
         respuesta = requests.get(f'{url}Obtener_Animes')
         respuesta.raise_for_status()
@@ -10,20 +15,76 @@ def obtener_animes():
     except requests.exceptions.RequestException as e:
         return e
 
-def obtener_descripcion():
-    pass
+def obtener_descripcion() -> dict:
+    """
+    Función que obtiene la descripción de un anime
+    Returns:
+        [json]: [json con la descripción del anime]
+    """
+    try:
+        name = str(input("Ingrese nombre del anime: "))
+        respuesta = requests.get(f'{url}descripcion', params={'name': name})
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        return e
 
-def obtener_clasificacion():
-    pass
+def obtener_clasificacion() -> dict:
+    """
+    Función que obtiene la clasificación de un anime
+    Returns:
+        [json]: [json con la clasificación del anime]
+    """
+    try:
+        name = input("Ingrese nombre del anime: ")
+        print(name)
+        respuesta = requests.get(f'{url}clasificacion', params={'name': name})
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        return e
 
-def obtener_categoria():
-    pass
+def obtener_categoria() -> dict:
+    """
+    Función que obtiene la categoría de un anime
+    Returns:
+        [json]: [json con la categoría del anime
+    """
+    try:
+        name = input("Ingrese nombre del anime: ")
+        respuesta = requests.get(f'{url}categoria', params={'name': name})
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        return e
 
-def obtener_estudio():
-    pass
+def obtener_estudio() -> dict:
+    """
+    Función que obtiene el estudio de un anime
+    Returns:
+        [json]: [json con el estudio del anime]
+    """
+    try:
+        name = input("Ingrese nombre del anime: ")
+        respuesta = requests.get(f'{url}estudio', params={'name': name})
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        return e
 
-def obtener_episodios():
-    pass
+def obtener_episodios() -> dict:
+    """
+    Función que obtiene los episodios de un anime
+    Returns:
+        [json]: [json con los episodios del anime]
+    """
+    try:
+        name = input("Ingrese nombre del anime: ")
+        respuesta = requests.get(f'{url}episodios', params={'name': name})
+        respuesta.raise_for_status()
+        return respuesta.json()
+    except requests.exceptions.RequestException as e:
+        return e
 
 def actualizar_episodios():
     pass
@@ -38,6 +99,9 @@ def eliminar_anime():
     pass
 
 def menu():
+    """
+    Funcion que muestra el menu de opciones
+    """
     try:
         while True:
             op = int(input("1. Obtener Animes\n2. Obtener descripcion\n3. Obtener clasificacion\n4. Obtener categoria\n5. Obtener estudio\n6. Obtener episodios\n7. Actualizar episodios\n8. Actualizar descripcion\n9. Actualizar nombre\n10. Eliminar anime\n11. Salir\nIngrese opcion: "))
--- servidor Api/anime.py
+++ servidor Api/anime.py
@@ -7,7 +7,12 @@ nombre_archivo = f"{ruta}anime.json"
 anime = abrir_archivo(nombre_archivo)
 
 @anime_router.get("/Obtener_Animes")
-def Obtener_Animes():
+def Obtener_Animes() -> list:
+    """
+    Obtiene todos los animes
+    Returns:
+        list: Lista de animes
+    """
     nombres = []
     for dic_anime in anime:
         nombre = dic_anime["name"]
@@ -15,33 +20,85 @@ def Obtener_Animes():
             nombres.append(nombre)
     return nombres
 
-def gets(columna,name):
+def gets(columna :str,name : str) -> str | int:
+    """
+    Obtiene el valor de una columna de un anime
+    Args:
+        columna (str): Nombre de la columna
+        name (str): Nombre del anime
+    Returns:
+        str | int: Valor de la columna
+    """
     for dic_anime in anime:
         if name == dic_anime["name"]:
             return dic_anime[columna]
     raise HTTPException(status_code=404, detail="Anime no encontrado")
 
 @anime_router.get("/descripcion")
-def descripcion(name):
+def descripcion(name : str) -> str:
+    """
+    Obtiene la descripcion de un anime
+    Args:
+        name (str): Nombre del anime
+    Returns:
+        str: Descripcion del anime
+    """
     return gets("description",name)
 
 @anime_router.get("/clasificacion")
-def clasificacion(name):
+def clasificacion(name : str):
+    """
+    Obtiene la clasificacion de un anime
+    Args:
+        name (str): Nombre del anime
+    Returns:
+        str: Clasificacion del anime
+    """
     return gets("Rating",name)
 
 @anime_router.get("/categoria")
-def categoria(name):
+def categoria(name : str) -> str:
+    """
+    Obtiene la categoria de un anime
+    Args:
+        name (str): Nombre del anime
+    Returns:
+        str: Categoria del anime
+    """
     return gets("categorie",name)
 
 @anime_router.get("/estudio")
-def estudio(name):
+def estudio(name : str) -> str:
+    """
+    Obtiene el estudio de un anime
+    Args:
+        name (str): Nombre del anime
+    Returns:
+        str: Estudio del anime
+    """
     return gets("studio",name)
 
 @anime_router.get("/episodios")
-def episodios(name):
+def episodios(name) -> int:
+    """
+    Obtiene los episodios de un anime
+    Args:
+        name (str): Nombre del anime
+    Returns:
+        int: Episodios del anime
+    """
     return gets("episode",name)
 
-def puts(columna,name,value):
+def puts(columna :str,name :str,value : str | int) -> str:
+    """
+    Actualiza el valor de una columna de un anime
+    Args:
+        columna (str): Nombre de la columna
+        name (str): Nombre del anime
+        value (str | int): Nuevo valor de la columna
+    Returns:
+        str: Mensaje de confirmacion
+    """
     for index, dic_anime in enumerate(anime):
         if name == dic_anime["name"]:
             anime[index][columna] = value
@@ -49,19 +106,50 @@ def puts(columna,name,value):
     raise HTTPException(status_code=404, detail="Anime no encontrado")
 
 @anime_router.put("/actualizar episodios")
-def actualizar_episodios(name: str,episode: int):
+def actualizar_episodios(name: str,episode: int) -> str:
+    """
+    Actualiza los episodios de un anime
+    Args:
+        name (str): Nombre del anime
+        episode (int): Nuevos episodios
+    Returns:
+        str: Mensaje de confirmacion
+    """
     return puts("episode",name,episode)
 
 @anime_router.put("/actualizar descripsion")
-def actualizar_episodios(name: str,Rating: float):
+def actualizar_episodios(name: str,Rating: float) -> str:
+    """
+    Actualiza la descripcion de un anime
+    Args:
+        name (str): Nombre del anime
+        Rating (float): Nueva descripcion
+    Returns:
+        str: Mensaje de confirmacion
+    """
     return puts("Rating",name,str(Rating))
 
 @anime_router.put("/actualizar nombre")
-def actualizar_nombre(name: str, new_name : str):
+def actualizar_nombre(name: str, new_name : str) -> str:
+    """
+    Actualiza el nombre de un anime
+    Args:
+        name (str): Nombre del anime
+        new_name (str): Nuevo nombre del anime
+    Returns:
+        str: Mensaje de confirmacion
+    """
     return puts("name",name,new_name)
 
 @anime_router.delete("/eliminar anime")
-def delete(name):
+def delete(name : str) -> str:
+    """
+    Elimina un anime
+    Args:
+        name (str): Nombre del anime
+    Returns:
+        str: Mensaje de confirmacion
+    """
     for index, dic_anime in enumerate(anime):
         if name == dic_anime["name"]:
             anime.pop(index)
